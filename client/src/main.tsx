d4be38c457fbb8e7799125f767c462d3bb989848
Daniel <danabelha1@gmail.com>
2026-04-22 19:23:32 -0300

    fix: adminpanel

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,21 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { trpc } from "./lib/trpc";
-import { httpBatchLink } from "@trpc/client";
+import { trpc, trpcClient } from "./lib/trpc";
+
 
 // 🔥 cria o client do react-query
 const queryClient = new QueryClient();
-
-// 🔥 cria o client do trpc
-const trpcClient = trpc.createClient({
-  links: [
-    httpBatchLink({
-      url: "https://site-casamento-meod.onrender.com/trpc", // backend
-    }),
-  ],
-});
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
