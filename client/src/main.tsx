b68cb6a93e2ebdb264ce24575c1ff801c11cecca
Daniel <danabelha1@gmail.com>
2026-04-27 21:19:54 -0300

    fix: adminpanel

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { trpc, trpcClient } from "./lib/trpc";
 
 
-// 🔥 cria o client do react-query
+// cria o client do react-query
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
