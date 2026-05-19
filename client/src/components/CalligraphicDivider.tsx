99be632187f31d39f054ef5e8c6f318482f47419
danabelha <222129940+danabelha@users.noreply.github.com>
2026-05-18 20:44:23 -0400

    style: update divider to be a full-width line in #462F29

--- client/src/components/CalligraphicDivider.tsx
+++ client/src/components/CalligraphicDivider.tsx
@@ -1,8 +1,6 @@
 
 export default function CalligraphicDivider({ className = "" }: { className?: string }) {
   return (
-    <div className={`flex justify-center items-center ${className}`}>
-      <div className="w-24 md:w-40 h-[1px] bg-[#462F29] opacity-40" />
-    </div>
+    <div className={`w-full h-[1px] bg-[#462F29]/20 ${className}`} />
   );
 }
